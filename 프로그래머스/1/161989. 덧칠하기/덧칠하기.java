cd71450a4caca56b4aadb8dcc0f8aa537ad35689
MiYoung Kim <asdf6812279@gmail.com>
2026-01-17 21:39:57 +0900

    [level 1] Title: 덧칠하기, Time: 1.46 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int end = 0;
+        
+        for(int i = 0; i < section.length; i++){
+            if(section[i] > end) {
+                answer++;
+                end = (section[i] + m) - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
