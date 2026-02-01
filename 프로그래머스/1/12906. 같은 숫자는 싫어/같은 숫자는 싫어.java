9c501581ecef93c0736b94e10ae65c16fa8fb267
MiYoung Kim <asdf6812279@gmail.com>
2026-02-01 17:25:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.06 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public Stack solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            if(stack.peek() != arr[i]) stack.push(arr[i]);
+        }
+
+        return stack;
+    }
+}
